7c6adcf5dc8af774d7252f2a4cefb92a5d34dec6
Bruno Marcenaro <bems2588@gmail.com>
2022-03-10 16:07:30 -0500

    add heroku

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/animals', (req, res) => {
   res.send('Hello!');
+});
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 function filterByQuery(query, animalsArray) {
